721aa0d35623339fbe283f366859e673fc1e4272
amanshri24 <83436731+amanshri24@users.noreply.github.com>
2023-12-23 23:15:34 +0530

    updated

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let option = document.querySelector(".options");
 let close = document.querySelector(".close");
 let popup = document.querySelector(".popup");
+let nav = document.querySelector("nav");
 
 option.addEventListener("click", function () {
   option.style.transform = "rotate(90deg)";
@@ -20,3 +21,24 @@ close.addEventListener("click", function () {
   option.style.opacity = "1";
   close.style.opacity = "0";
 });
+
+let lastscroll = 0;
+
+document.addEventListener("scroll", function () {
+  if (window.scrollY > 350) {
+    if (window.scrollY > lastscroll) {
+      nav.style.transform = "translateY(-80px)"; 
+      popup.style.display = "none";
+      option.style.transform = "rotate(-90deg)";
+      close.style.zIndex = "999";
+      option.style.zIndex = "9999";
+      option.style.opacity = "1";
+      close.style.opacity = "0";
+    } else {
+      nav.style.transform = "translateY(0px)";
+    }
+  } else {
+    nav.style.transform = "translateY(0px)";
+  }
+  lastscroll = window.scrollY;
+})
